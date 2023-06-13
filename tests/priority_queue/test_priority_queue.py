3651868437f30d53c56693a18530548a0e3fa7c0
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-13 16:33:46 -0300

    criando base do teste

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,11 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    teste_fila = PriorityQueue()
+    teste_fila.enqueue("arquivo1.txt", 1)
+    teste_fila.enqueue("arquivo2.txt", 6)
+    teste_fila.enqueue("arquivo3.txt", 3)
+    assert teste_fila._data[0]["nome_do_arquivo"] == "arquivo3.txt"
+    assert teste_fila._data[1]["nome_do_arquivo"] == "arquivo2.txt"
+    assert teste_fila._data[2]["nome_do_arquivo"] == "arquivo1.txt"

f565b9dd30bc8f57483b0f32a9a70895154480b6
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-13 15:14:03 -0300

    criando começo do process, importando o txt_importer para usar

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if instance.search(path_file):
+        return None
+
+    imported_file = txt_importer(path_file)
+
+    created_obj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(imported_file),
+        "linhas_do_arquivo": imported_file,
+    }
 
 
 def remove(instance):

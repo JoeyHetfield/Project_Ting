a185b57f89ebc3111c68806d601f242f197e8d23
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-13 17:04:45 -0300

    feito base da exists_word, mas retorno ainda não está sendo como deveria

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    exist = []
+
+    for item in range(len(instance._data)):
+        file = instance.search(item)
+        filename = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+        occurrences = []
+
+        for line in range(len(lines)):
+            if word in lines[line].lower():
+                occurrences.append(line)
+
+        if len(occurrences) > 0:
+            exist.append(
+                {
+                    "palavra": word,
+                    "nome_do_arquivo": filename,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return exist
 
 
 def search_by_word(word, instance):

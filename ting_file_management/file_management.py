58eb65711007789148b668c4bdb7379d719bded2
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-13 14:19:34 -0300

    base do req2, funcionou quando arquivo termina com diferente de txt, tive que importar sys

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return None
+
+    if not path_file.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado")
+        return None

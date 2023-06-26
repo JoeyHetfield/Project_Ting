d5bd62f034bb306981b377ac8dceb4d9229d6d16
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-26 16:42:53 -0300

    add repo

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -25,7 +25,30 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    exist = exists_word(word, instance)
+    word = word.lower()
+    exist = []
 
-    if len(exist) == 0:
-        return list()
+    for item in range(len(instance._data)):
+        file = instance.search(item)
+        filename = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+        occurrences = []
+
+        for line in range(len(lines)):
+            if word in lines[line].lower():
+                occurrences.append(
+                    {
+                        "linha": line + 1,
+                    }
+                )
+
+        if len(occurrences) > 0:
+            exist.append(
+                {
+                    "palavra": word,
+                    "arquivo": filename,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return exist
